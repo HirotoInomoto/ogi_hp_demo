e26a26dffea416838859544b8ac638936aac0994
inomoto <hiroto_inomoto@demia.co.jp>
2021-11-01 20:03:22 +0900

    info

--- js/info.js
+++ js/info.js
@@ -76,3 +76,28 @@ if(userAgent == "pc"){
     document.querySelector('.map__wrapper').style.width = imageWidth + "px";
   })
 }
+
+/**
+* タッチ操作での拡大縮小禁止
+*/
+function no_scaling() {
+  document.addEventListener("touchmove", mobile_no_scroll, { passive: false });
+}
+
+/**
+* タッチ操作での拡大縮小禁止解除
+*/
+function return_scaling() {
+  document.removeEventListener('touchmove', mobile_no_scroll, { passive: false });
+}
+
+/**
+* 拡大縮小禁止
+*/
+function mobile_no_scroll(event) {
+  // ２本指での操作の場合
+  if (event.touches.length >= 2) {
+      // デフォルトの動作をさせない
+      event.preventDefault();
+  }
+}
