2042e30ebe41dd1b31a0b788f854afebca163f1e
inomoto <hiroto_inomoto@demia.co.jp>
2021-11-01 20:08:14 +0900

    info

--- js/info.js
+++ js/info.js
@@ -77,27 +77,13 @@ if(userAgent == "pc"){
   })
 }
 
-/**
-* タッチ操作での拡大縮小禁止
-*/
-function no_scaling() {
-  document.addEventListener("touchmove", mobile_no_scroll, { passive: false });
-}
-
-/**
-* タッチ操作での拡大縮小禁止解除
-*/
-function return_scaling() {
-  document.removeEventListener('touchmove', mobile_no_scroll, { passive: false });
-}
-
-/**
-* 拡大縮小禁止
-*/
-function mobile_no_scroll(event) {
-  // ２本指での操作の場合
-  if (event.touches.length >= 2) {
-      // デフォルトの動作をさせない
-      event.preventDefault();
+document.body.addEventListener("touchstart", function(e){
+  if (e.touches && e.touches.length > 1) {
+    e.preventDefault();
   }
-}
+}, {passive: false});
+document.body.addEventListener("touchmove", function(e){
+  if (e.touches && e.touches.length > 1) {
+    e.preventDefault();
+  }
+}, {passive: false});
